eef10deb13c6dccb907b15e2abbfc783b220da6d
MajkowskiDylan <dylanmaj@free.fr>
2020-03-12 22:48:12 +0100

    Initial commit

---
+++ src/main/java/com/momcgl/cglmom/CglmomApplication.java
@@ -0,0 +1,13 @@
+package com.momcgl.cglmom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CglmomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CglmomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/momcgl/cglmom/CglmomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momcgl.cglmom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CglmomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
